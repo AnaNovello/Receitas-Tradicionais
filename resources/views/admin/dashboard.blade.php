dfea38c8f12d79a9f4d09390a37a0e0736385b8c
Ana Leticia <analeticia380s@gmail.com>
2025-02-19 22:49:16 -0400

    feat: adicionados novos recursos na dashboard do admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,4 @@
+<link rel = "stylesheet" type="text/css" href="./css/styles_admin_dashboard.css">
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -30,5 +31,66 @@
             </div>
         </div>
     </div>
+
+    <div class="py-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <h2 class="text-2xl font-bold mb-4"> Gerenciar Receitas </h2>
+
+                    <div class="flex space-x-4">
+                        <a href="" class="flex-1 aspect-square flex items-center justify-center font-bold rounded shadow">
+                            Adicionar Nova
+                        </a>
+                        <a href="" class="flex-1 aspect-square flex items-center justify-center font-bold rounded shadow">
+                            Visualizar Aprovadas
+                        </a>
+                        <a href="" class="flex-1 aspect-square flex items-center justify-center font-bold rounded shadow">
+                            Atualizar Existente
+                        </a>
+                        <a href="" class="flex-1 aspect-square flex items-center justify-center font-bold rounded shadow">
+                            Excluir
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <h2 class="text-2xl font-bold mb-4"> Status de Envios</h2>
+
+                    <div class="flex space-x-4">
+                        nome da receita | descrição | anexo | status | feedback
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <h2 class="text-2xl font-bold mb-4"> Pedidos de usuários </h2>
+
+                    <div class="flex space-x-4">
+                        id do solicitante | data de solicitação | título do envio | descrição | anexo
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Espaço extra na mesma cor de fundo (cinza) -->
+    <div class="h-16 bg-gray-100"></div>
 
 </x-app-layout>
