1305869c445c69b714a8ce8e1f2e45e71cacc863
Ana <analeticia380s@gmail.com>
2025-03-24 09:35:56 -0400

    style: modificação de texto das regiões

--- resources/views/categorias.blade.php
+++ resources/views/categorias.blade.php
@@ -3,61 +3,70 @@
 @section('title', 'Categorias')
 
 @section('content')
-    
-    <link rel = "stylesheet" type="text/css" href="{{ asset('css/styles_categorias.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles_categorias.css') }}">
     <header class="header_categorias">
         <main class="categorias">
             <div class="header_3">
-                <div class="search-container">
-                    <input type="text" id="search" placeholder="Buscar" onkeyup="filtrarReceitas()">
+                <div class="maior_acesso">
+                    <ul>
+                        <li>
+                            <b class="texto_rank" href="">Regiões do Brasil</b>
+                        </li>
+                    </ul>
                 </div>
                 
-                <div class="slider_m_acesso">
-                    <div class="slides">
-                        <input type="radio" name="radio-btn" id="radio1">
-                        <input type="radio" name="radio-btn" id="radio2">
-                        <input type="radio" name="radio-btn" id="radio3">
-                        <input type="radio" name="radio-btn" id="radio4">
-                        <input type="radio" name="radio-btn" id="radio5">
-                        
-                        <div class="slide first">
-                            <img src="/img/categorias/Norte.png" alt="img" />
-                        </div>
-
-                        <div class="slide">
-                            <img src="/img/categorias/Nordeste.png" alt="img" />
-                        </div>
-
-                        <div class="slide">
-                            <img src="/img/categorias/Centro_Oeste.png" alt="img" />
-                        </div>
-
-                        <div class="slide">
-                            <img src="/img/categorias/Sudeste.png" alt="img" />
-                        </div>
-
-                        <div class="slide">
-                            <img src="/img/categorias/Sul.png" alt="img" />
-                        </div>
-
-                        <div class="nav_auto">
-                            <div class="auto_btn1"></div>
-                            <div class="auto_btn2"></div>
-                            <div class="auto_btn3"></div>
-                            <div class="auto_btn4"></div>
-                            <div class="auto_btn5"></div>
-                        </div>
-                    </div>
-
-                    <div class="nav_manual">
-                        <label for="radio1" class="manual-btn"></label>
-                        <label for="radio2" class="manual-btn"></label>
-                        <label for="radio3" class="manual-btn"></label>
-                        <label for="radio4" class="manual-btn"></label>
-                        <label for="radio5" class="manual-btn"></label>
-                    </div>
-
+                <div class="capa">
+                    <img src="/img/categorias/Norte.png" alt="img" />
                 </div>
+
+                <div class="maior_acesso">
+                    <div class="sobre_container">
+                        <header class="apresentacao">
+                            <p class="p_1">
+                                Tendo sido a primeira região efetivamente colonizada por portugueses, ainda no 
+                                século XVI, que aí encontraram as populações nativas e foram acompanhados por 
+                                africanos trazidos como escravos, a cultura nordestina é bastante particular e típica, 
+                                apesar de extremamente variada. Sua base é luso-brasileira, com grandes influências africanas, 
+                                em especial na costa de Pernambuco à Bahia e no Maranhão, e ameríndias, em especial no sertão semiárido. 
+                            </p>  
+                            <p class="p_1">
+                                A culinária nordestina é variada, refletindo as condições econômicas e produtivas das diversas 
+                                paisagens geoeconômicas dessa região. Frutos do mar e peixes são bastante utilizados na culinária 
+                                do litoral, enquanto, no sertão, predominam receitas que utilizam a carne e derivados do gado 
+                                bovino, caprino e ovino. Ainda assim, há várias diferenças regionais, tanto na variedade de 
+                                pratos quanto em sua forma de preparo. Por exemplo, no Ceará, predomina o mugunzá - também 
+                                chamado macunzá ou mucunzá - salgado, enquanto em Pernambuco predomina o doce. 
+                                Na Bahia, os principais destaques são as comidas feitas com azeite de dendê e com camarão, 
+                                como as moquecas, o vatapá, o acarajé e os bobós; porém não são menos apreciadas comidas 
+                                acompanhadas de pirão como mocotó e rabada, além de doces como a cocada, a rapadura e o alfenim, 
+                                que estão presentes em todo o nordeste. No Maranhão, destacam-se o cuxá, o arroz de cuxá, o bobó, 
+                                o peixe pedra e a torta de camarão. Também no Maranhão se destaca o Guaraná Jesus, patrimônio maranhense 
+                                de fama nacional. Já o bolo de rolo é patrimônio imaterial de Pernambuco.
+                            </p>
+                            <p class="p_1">
+                                Algumas comidas típicas da região são: o baião de dois, a carne de sol, o queijo de coalho, o vatapá, 
+                                o acarajé, a panelada, a buchada, a canjica, o feijão e arroz de coco, o feijão verde e o sururu, assim 
+                                como vários doces feitos de mamão, jaca, abóbora, laranja, etc. Algumas frutas regionais - não necessariamente 
+                                nativas da região - são a ciriguela, o cajá, o buriti, a cajarana, o umbu, a macaúba, juçara, bacuri, cupuaçu, 
+                                buriti, murici e a pitomba, além de outras. 
+                            </p>
+                        </header>
+                    </div> 
+                </div>
+
+                <div class="capa">
+                    <img src="/img/categorias/Nordeste.png" alt="img" />
+                </div>
+                <div class="capa">
+                    <img src="/img/categorias/Centro_Oeste.png" alt="img" />
+                </div>
+                <div class="capa">
+                    <img src="/img/categorias/Sudeste.png" alt="img" />
+                </div>
+                <div class="capa">
+                    <img src="/img/categorias/Sul.png" alt="img" />
+                </div>
+
             </div>
             <script src=" {{ asset('js/slider_index.js') }}" ></script>
         </main>
